8fccb7e7d1844615791a33c7462b8ab68d0176de
NemanjaP83 <necaintruder@gmail.com>
2022-02-24 22:09:55 +0100

    resolveproblem with style

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import htmlPurge from 'vite-plugin-html-purgecss'
+// import htmlPurge from 'vite-plugin-html-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), htmlPurge()],
+  plugins: [react()],
 })
