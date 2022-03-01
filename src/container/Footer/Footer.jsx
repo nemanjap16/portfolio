e490d5a578e687de8531efdd278f914bd6a54964
NemanjaP83 <necaintruder@gmail.com>
2022-03-01 15:43:00 +0100

    added emailjs

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,32 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import emailjs from '@emailjs/browser'
+
 import { images } from '../../constants'
 import { AppWrapper, MotionWrapper } from '../../hoc'
 // Style
 import './Footer.scss'
 
+// Validation schema
+const Schema = yup
+  .object()
+  .shape({
+    name: yup.string().min(3).required(),
+    email: yup.string().email().required(),
+    message: yup.string().min(10).required(),
+  })
+  .required()
+
+const config = {
+  service_id: 'service_eylz1va',
+  template_id: 'template_5fmj3m8',
+  user_id: 'user_DqyHZfmHeguNW90Gr1Jmi',
+}
+
 const Footer = () => {
-  // const [formData, setFormData] = useState({ name: '', email: '', message: '' })
-  // const [isFormSubmitted, setIsFormSubmitted] = useState(false)
-  // const [loading, setLoading] = useState(false)
+  /**
+   * @description - Use Form Hook with validation schema
+   */
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    resolver: yupResolver(Schema),
+  })
 
-  // const { username, email, message } = formData
+  const [isLoading, setisLoading] = useState(false)
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' })
 
-  // const handleChangeInput = (e) => {
-  //   const { name, value } = e.target
-  //   setFormData({ ...formData, [name]: value })
-  // }
+  const sendEmail = (formData) => {
+    setisLoading(true)
+    // Send email
+    emailjs
+      .send(config.service_id, config.template_id, formData, config.user_id)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text)
+      })
+      .catch((err) => {
+        console.log('FAILED...', err)
+      })
 
-  // const handleSubmit = () => {
-  //   setLoading(true)
+    setTimeout(() => {
+      setisLoading(false)
+      reset()
+    }, 2000)
+  }
 
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //     setIsFormSubmitted(true)
-  //   }, 2000)
-  // }
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
   return (
-    <>
+    <div className="form__style">
       <h2 className="head-text">
         Contact <span>me</span>
       </h2>
@@ -45,47 +89,61 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      {/* {!isFormSubmitted ? (
+
+      <form onSubmit={handleSubmit(sendEmail)}>
         <div className="app__footer-form app__flex">
           <div className="app__flex">
             <input
               className="p-text"
               type="text"
               placeholder="Your Name"
-              name="username"
-              value={username}
+              name="name"
+              value={formData.name}
+              {...register('name')}
               onChange={handleChangeInput}
             />
           </div>
+          <span style={{ color: 'red', marginRight: 'auto' }}>
+            {errors.name?.message}
+          </span>
           <div className="app__flex">
             <input
               className="p-text"
               type="email"
               placeholder="Your Email"
               name="email"
-              value={email}
+              value={formData.email}
+              {...register('email')}
               onChange={handleChangeInput}
             />
           </div>
+          <span role="alert" style={{ color: 'red', marginRight: 'auto' }}>
+            {errors.email?.message}
+          </span>
           <div>
             <textarea
               className="p-text"
               placeholder="Your Message"
-              value={message}
               name="message"
+              value={formData.message}
+              {...register('message')}
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
-            {!loading ? 'Send Message' : 'Sending...'}
+          <span style={{ color: 'red', marginRight: 'auto' }}>
+            {errors.message?.message}
+          </span>
+
+          <button
+            type="submit"
+            className="p-text"
+            style={{ backgroundColor: '#313bac' }}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </div>
-      ) : (
-        <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
-        </div>
-      )} */}
-    </>
+      </form>
+    </div>
   )
 }
 
